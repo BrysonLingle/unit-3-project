28556af105f63e8a3d07fb68a5bb65dbf5f7ee2d
Bryson lingle <brysonburkett99@gmail.com>
2023-09-05 17:37:20 -0600

    planning md

--- planning.md
+++ planning.md
@@ -1,9 +1,16 @@
+# NFL Stats
+* Solo Project
+# Product Objective
+* The Objective of my app is a easy place to find all of the nfl teams and some basic inforamtion on them all in one area. 
 # Features 
 * Deployed on Netlify
 * Home Page with team cards
 * As user clicks on card the team info will show
 * Pictures of team on card potentially
-* 
+# Data Requirements
+* I will have to use an API for this project. Which has yet to be enabled.
+# Deployment
+* Deployment will be on netlify by Monday The 11th.
 # WireFrame
 * ![wireframe](<Screenshot 2023-09-04 at 2.14.51 PM.png>)
 # Trello
--- src/Teams.js
+++ src/Teams.js
@@ -1,47 +1,23 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+// import axios from "axios";
 
-function Teams() {
-  const [teamStats, setTeamStats] = useState([]);
-  const [loading, setLoading] = useState(true);
+// function Teams() {
+//   const [teamStats, setTeamStats] = useState([]);
+//   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        // Replace with the new API endpoint and key
-        const response = await axios.get(
-          "https://nfl-team-stats1.p.rapidapi.com/teamStats",
-          {
-            headers: {
-              "x-rapidapi-host": "nfl-team-stats1.p.rapidapi.com",
-              "x-rapidapi-key": "c6dbe658b5mshf53b66a8dd1955cp19f43ajsnb8ec20c4af94",
-            },
-          }
-        );
-
-        if (response.data && response.data.results) {
-          const standingsData = response.data.results[0].league.standings[0];
-          setTeamStats(standingsData.map((team) => team.team));
-        } else {
-          console.error("No 'results' property found in API response:", response.data);
-          setTeamStats([]);
-        }
-
-        setLoading(false);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, []);
+  export default function Teams() {
+    const [teamStats, setTeamStats] = useState([])
+    useEffect(() => {
+        fetch('https://site.api.espn.com/apis/site/v2/sports/football/nfl/teams')
+            .then(response => response.json())
+            .then(data => setTeamStats(data))
+            .catch(console.error)
+    }, []);
 
   return (
     <div className="container">
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
+  
+       : (
         <div className="team-list">
           {teamStats.map((team, index) => (
             <div className="card" key={index}>
@@ -56,9 +32,8 @@ function Teams() {
             </div>
           ))}
         </div>
-      )}
+      )
     </div>
   );
 }
 
-export default Teams;
