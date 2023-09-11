585637bf735b0073c7f2dfa7bc22c168d8e4edf5
Bryson lingle <brysonburkett99@gmail.com>
2023-09-11 08:46:15 -0600

    fixing footer

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className="bg-gray-800 text-white py-4 absolute bottom-0 w-full">
+    <footer className="bg-gray-800 text-white py-4 relative bottom-0 w-full">
       <div className="container mx-auto text-center">
         &copy; {new Date().getFullYear()} NFL Stats by Bryson Lingle. My Personal GitHub: 
         <a href="https://github.com/BrysonLingle" className="text-blue-400 hover:underline" target="_blank" rel="noopener noreferrer">
